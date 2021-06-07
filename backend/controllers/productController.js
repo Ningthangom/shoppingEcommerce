7ed7ac0b9b8ce5c36cde615154b58f296c8ff048
Ningthangom <ningthangom@gmail.com>
2021-06-08 09:07:13 +0930

    80% of back end is done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -101,45 +101,46 @@ const APIFeatures = require('../utils/apiFeatures')
         })
 
 
-        // create new reviews => api/v1/ reviews
+// Create new review   =>   /api/v1/review
+exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
 
-        exports.createProductReview = catchAsyncErrors (async (req, res, next) => {
-            const {rating, comment, productId} = req.body;
+    const { rating, comment, productId } = req.body;
 
-            const review = {
-                user:req.user._id,
-                name: req.user.name,
-                rating:Number(rating),
-                comment
+    const review = {
+        user: req.user._id,
+        name: req.user.name,
+        rating: Number(rating),
+        comment
+    }
+
+    const product = await Product.findById(productId);
+
+    const isReviewed = product.reviews.find(
+        r => r.user.toString() === req.user._id.toString()
+    )
+
+    if (isReviewed) {
+        product.reviews.forEach(review => {
+            if (review.user.toString() === req.user._id.toString()) {
+                review.comment = comment;
+                review.rating = rating;
             }
-
-            // update the review
-            const product = await Product.findById(productId);
-            //check if the product is reviewed
-            const isReviewed = product.reviews.find( 
-                r => r.user.toString() === req.user._id.toString()
-            )
-            if (isReviewed){
-                    product.reviews.forEach(review => {
-                        if(review.user.toString() === req.user._id.toString()){
-                            review.comment = comment;
-                            review.rating = rating;
-                        }
-                    })
-            }else{
-                product.reviews.push(review);
-                product.numOfReviews = product.reviews.length
-            }
-            //calculating average rating 
-            product.ratings = product.reviews.reduce((acc, item)=> item.rating+acc, 0)/product.reviews.length;
-
-            await product.save({ validateBeforeSave: false}); 
-
-            res.status(200).json({
-                success: true,
-            })
         })
 
+    } else {
+        product.reviews.push(review);
+        product.numOfReviews = product.reviews.length
+    }
+
+    product.ratings = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length
+
+    await product.save({ validateBeforeSave: false });
+
+    res.status(200).json({
+        success: true
+    })
+
+})
 
         // get product reviews => /api/v1/reviews
 
@@ -157,19 +158,18 @@ const APIFeatures = require('../utils/apiFeatures')
     exports.deleteProductReviews = catchAsyncErrors (async (req, res, next) => {
                 const product = await Product.findById(req.query.productId);
                 
-               /*  console.log(product) */
+                console.log(product);
+               const reqid = req.query;
+               /*  console.log(reviews); */
+                console.log(`this is query${reqid}`);
 
-                const reviews = product.reviews.filter(review => review._id.toString() !== req.query.id.toString( ));
+                const reviews = product.reviews.filter(review => review._id.toString() !== req.query.id.toString());
                 
-                const reqid = req.query.id;
-                console.log(reviews);
-                console.log(reqid);
-
                 const numOfReviews = reviews.length;
               
                 const ratings = product.reviews.reduce((acc, item)=> item.rating+acc, 0)/reviews.length;
                 
-                await Product.findById(req.query.id, {
+                await Product.findByIdAndUpdate(req.query.id, {
                     reviews,
                     ratings,
                     numOfReviews
